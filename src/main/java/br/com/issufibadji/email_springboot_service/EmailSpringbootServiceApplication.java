7638e1c6abcf948f0b4acd5ba31942649adca5ce
Issufi Badji <issufibadji@gmail.com>
2026-04-26 20:43:22 -0300

    sender email

---
+++ src/main/java/br/com/issufibadji/email_springboot_service/EmailSpringbootServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.issufibadji.email_springboot_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSpringbootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSpringbootServiceApplication.class, args);
+	}
+
+}
